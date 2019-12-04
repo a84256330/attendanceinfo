10202511a249c8b0d05562a91ea599119c757e5b
a84256330 <mc5588zx@126.com>
2019-12-04 18:09:23 +0800

    Initial commit

---
+++ src/test/java/com/ma/attendanceinfo/AttendanceinfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ma.attendanceinfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttendanceinfoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
